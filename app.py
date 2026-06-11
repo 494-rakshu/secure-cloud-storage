ea575b58057da1dbfe609777dc56cfa3c7508a76
Rakshitha Reddy <brakshitareddy1234@gmail.com>
2026-06-11 20:11:13 +0530

    Day 2 and Day 3: Authentication, Sessions and Password Hashing

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, session
+from werkzeug.security import generate_password_hash, check_password_hash
 import sqlite3
 
 app = Flask(__name__)
+app.secret_key = "secure_cloud_storage"
+
 
 @app.route("/")
 def home():
@@ -16,13 +19,14 @@ def register():
         name = request.form["name"]
         email = request.form["email"]
         password = request.form["password"]
+        hashed_password = generate_password_hash(password)
 
         conn = sqlite3.connect("database.db")
         cursor = conn.cursor()
 
         cursor.execute(
             "INSERT INTO users(name,email,password) VALUES(?,?,?)",
-            (name, email, password)
+            (name, email, hashed_password)
         )
 
         conn.commit()
@@ -30,8 +34,57 @@ def register():
 
         return "Registration Successful!"
 
-    return render_template("register.html")
+    return render_template("register.html")  
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+
+    if request.method == "POST":
+
+        email = request.form["email"]
+        password = request.form["password"]
+
+        conn = sqlite3.connect("database.db")
+        cursor = conn.cursor()
+
+        cursor.execute(
+            "SELECT * FROM users WHERE email=?",
+            (email,)
+        )
+
+        user = cursor.fetchone()
+
+        conn.close()
+
+        if user and check_password_hash(user[3], password):
+
+            session["user_name"] = user[1]
+
+            return redirect("/dashboard")
+
+            return "Invalid Email or Password"
+        
+
+    return render_template("login.html")
+
+
+@app.route("/dashboard")
+def dashboard():
+
+    if "user_name" not in session:
+        return redirect("/login")
+
+    return render_template(
+        "dashboard.html",
+        user_name=session["user_name"]
+    )
+
+@app.route("/logout")
+def logout():
+
+    session.pop("user_name", None)
+
+    return redirect("/login")
 
 @app.route("/users")
 def users():
